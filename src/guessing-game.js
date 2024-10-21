48688bf42d8659ffcfeeb7e2b0e2c37f3cac389f
RamanPr5 <nattibampo5@mail.ru>
2024-10-21 20:55:34 +0200

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+        this.min = min;
+        this.max = max;
+      }
+    
+      guess() {
+        this.assumption = Math.round((this.max + this.min) / 2);
+        return this.assumption;
+      }
+    
+      lower() {
+        return this.max = this.guess();
+      }
+    
+      greater() {
+        return this.min = this.guess();
+      }
+    
 }
 
 module.exports = GuessingGame;
